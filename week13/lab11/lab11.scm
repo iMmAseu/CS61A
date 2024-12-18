f8273bd05f9db923f9e769c1f3724c3c705e625e
iMmAseu <101717526+iMmAseu@users.noreply.github.com>
2024-12-18 23:04:19 +0800

    CS61A lab11 finished

--- week13/lab11/lab11.scm
+++ week13/lab11/lab11.scm
@@ -1,15 +1,27 @@
 (define (if-program condition if-true if-false)
-  'YOUR-CODE-HERE)
+  (cons 'if
+        (cons condition
+              (cons if-true (cons if-false nil)))))
 
 (define (square n) (* n n))
 
-(define (pow-expr base exp) 'YOUR-CODE-HERE)
+(define (pow-expr base exp)
+  (cond 
+    ((= exp 0)
+     1)
+    ((= exp 1)
+     (cons '* (cons base (cons 1 nil))))
+    ((even? exp)
+     (list 'square (pow-expr base (/ exp 2))))
+    (else
+     (cons '*
+           (cons base (cons (pow-expr base (- exp 1)) nil))))))
 
 (define-macro (repeat n expr)
-  `(repeated-call ,n ___))
+  `(repeated-call ,n (lambda () ,expr)))
 
 ; Call zero-argument procedure f n times and return the final result.
 (define (repeated-call n f)
   (if (= n 1)
-      ___
-      (begin ___ ___)))
+      (f)
+      (begin (f) (repeated-call (- n 1) f))))
