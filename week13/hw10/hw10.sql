0c5904d54aac725ef4211a8c16103594b96e1147
iMmAseu <101717526+iMmAseu@users.noreply.github.com>
2024-12-19 10:31:16 +0800

    CS61A lab12 & hw 10 didn't finish yet

--- week13/hw10/hw10.sql
+++ week13/hw10/hw10.sql
@@ -26,24 +26,35 @@ CREATE TABLE sizes AS
 
 -- All dogs with parents ordered by decreasing height of their parent
 CREATE TABLE by_parent_height AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+    SELECT p.child AS child
+    FROM parents AS p
+    JOIN dogs AS d
+    ON p.parent = d.name
+    ORDER BY d.height DESC;
 
 
 -- The size of each dog
 CREATE TABLE size_of_dogs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT dogs.name, sizes.size
+  FROM dogs
+  JOIN sizes
+  ON dogs.height > sizes.min AND dogs.height <= sizes.max;
 
 
 -- [Optional] Filling out this helper table is recommended
 CREATE TABLE siblings AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.child AS first, b.child AS second FROM parents AS a, parents AS b
+    WHERE a.parent = b.parent AND a.child < b.child;
 
 -- Sentences about siblings that are the same size
 CREATE TABLE sentences AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT "The two siblings, " || first || " and " || second || ", have the same size: " || a.size
+    FROM siblings, size_of_dogs AS a, size_of_dogs AS b
+    WHERE a.size = b.size AND a.name = first AND b.name = second;
 
 
 -- Height range for each fur type where all of the heights differ by no more than 30% from the average height
 CREATE TABLE low_variance AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT SELECT fur, MAX(height) - MIN(height) AS height_range FROM dogs GROUP BY fur
+      HAVING MIN(height) >= .7 * AVG(height) AND MAX(height) <= 1.3 * AVG(height);
 
---
+++ week15/web_for_lab12/Lab 12_ SQL _ CS 61A Fall 2024_files/code_tabindex.js.下载
@@ -0,0 +1,10 @@
+// adds tabindex = 0 to code elements inside pre elements to make them keyboard accessible
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Select all code elements inside pre tags
+    const codeBlock = document.querySelectorAll('pre code');
+
+    // Add tabindex="0" to each selected element
+    codeBlock.forEach(element => {
+        element.setAttribute('tabindex', '0');
+    });
+});
